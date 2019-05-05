49b7527bb97582e903160367707d81c038e4cdc7
zx <petsgre@gmail.com>
2019-05-05 12:19:31 +0800

    practice typescript feature

--- src/module/test.ts
+++ src/module/test.ts
@@ -1,6 +1,40 @@
 import ok = require('./module')
-console.log(ok)
+type CardinalDirection = 'North' | 'East' | 'South' | 'West';
+
+function move(distance: number, direction: CardinalDirection) {
+  // ...
+}
+
+move(1, 'North'); // ok
+interface obj {
+  name: string
+}
+type My = obj
+const name: My = {
+  name: 'zx'
+}
+
+type Adder = (a: number, b: number) => number;
+let foo: Adder = (a, b) => a + b;
+foo(1,2)
+console.log(name)
+console.log("**********")
 import axios = require('../util/axios.js')
+import axios1 = require('../util/axios.js')
+console.log(axios1)
+namespace Utility {
+  export function log(msg: string) {
+    console.log(msg);
+  }
+  export function error(msg: string) {
+    console.log(msg);
+  }
+}
+window.helloWorld = () => console.log('hello world');
+window.helloWorld();
+// usage
+Utility.log('Call me');
+Utility.error('maybe');
 const cities = axios.get('http://192.168.199.195:9999/cities')
 cities.then((res: any) => {
   console.log(res.data)
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,12 @@
+interface Window {
+  helloWorld(): void;
+}
 declare var require:any
+declare var global:Array<any>
+declare module 'dist/module/foo' {
+  // some variable declarations
+  export var bar: number;
+}
+declare var $:any
+declare var jQuery:any
+declare var window: Window;
